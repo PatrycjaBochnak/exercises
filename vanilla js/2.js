b8cfd178ac5ddf2e295e1c2e0b6339ffe4094eba
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-14 20:17:00 +0100

    2nd and 3rd task

--- vanilla js/2.js
+++ vanilla js/2.js
@@ -1,2 +1,4 @@
 let x = 1 
 let y = 2 
+
+console.log(`Hej to jest cyfra ${x} a to cyfra ${y}`);
---
+++ vanilla js/3-repairingMistakes.js
@@ -0,0 +1,16 @@
+var firstName = 'Ania'
+let firstName = 'Tomek'
+
+var age = 18
+let age = 12
+let age2 = 24
+ 
+var favColor = 'niebieski'
+let favColor = 'czerwony'
+
+
+const favMeal = 'schabowy'
+ 
+let currentCar = 'Audi'
+ 
+
