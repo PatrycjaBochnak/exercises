9531a18111bbdba1fb26a6970952d30d41466c47
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-16 22:00:01 +0100

    5

--- vanilla js/5-operatory.js
+++ vanilla js/5-operatory.js
@@ -10,3 +10,13 @@ console.log(x);
 x+= y;
 
 console.log(x);
+
+const pass = "hhufdhsfds";
+
+if (pass.length > 10 && pass.includes("!")) {
+    console.log("Masz rewelacyjne hasło")
+} else if (pass.length > 10) {
+    console.log("Masz dobre hasło")
+} else {
+    console.log("Masz za krótkie hasło")
+}
