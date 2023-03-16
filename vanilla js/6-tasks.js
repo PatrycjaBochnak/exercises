aafecd2759ee2d44b4edb8e4f105625573aaf0ff
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-16 22:19:01 +0100

    6

--- vanilla js/6-tasks.js
+++ vanilla js/6-tasks.js
@@ -1,9 +1,9 @@
-const x = 50;
-const y = 30;
+// const x = 50;
+// const y = 30;
 
-if (x > y) {
-    console.log(`${x} jest większe od ${y}`)
-};
+// if (x > y) {
+//     console.log(`${x} jest większe od ${y}`)
+// };
 
 const color = blue;
 const newColor = green;
@@ -13,3 +13,14 @@ if (color === newColor) {
 } else {
     console.log("Kolory się nie zgadzają")
 };
+
+const x = 100;
+const y = 50;
+
+if (x > y) {
+    console.log("x jest większy")
+} else if (x == y){
+    console.log("x jest równy")
+} else {
+    console.log ("x jest mniejszy")
+}
