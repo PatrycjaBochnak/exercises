5c4e830fcc993d99efb9aff5634291a900990758
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-19 14:35:58 +0100

    8

--- vanilla/8-loops-tasks.js
+++ vanilla/8-loops-tasks.js
@@ -3,3 +3,10 @@ const towns = ["Gdynia", "Sopot", "Gdańsk"];
 for (let i = 0; i < towns.length; i++) {
     console.log(`To miasto nazywa się: ${towns[i]}`)
 }
+
+let x = 0;
+
+while (x < 10) {
+    x = x + 2
+    console.log(x)
+}
