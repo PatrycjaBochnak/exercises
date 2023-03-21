2228cf76d0c5c70b898d02942dccbc9a62e4ce78
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-21 13:38:28 +0100

    arrays

--- vanilla/9-arrays.js
+++ vanilla/9-arrays.js
@@ -2,3 +2,16 @@
 // shift - usuwa pierwszy element tablicy 
 // push - dodaje elementy na końcu tablicy
 // pop - usuwa ostatni element tablicy
+// concat - łączenie dwóch tablic
+
+// // function map
+// - podobna do pętli, wykonuje jakoś kod na kadym elemencie tablicy
+// - w nawiasach podajemy nazwę funkcji, której kod ma się wykonać na elementach tablicy
+// - zwraca nową tablicę
+
+// spread operator - dajemy trzy kropki przed nazwą tablicy i wtedy mamy osobne stringi
+
+const drinks = ["pepsi", "kawa", "sok"]
+const meals = ["schabowy", "spaghetti", "zupa"]
+
+console.log(...drinks, ...meals)
