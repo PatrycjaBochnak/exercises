0f3d3486200bed4537138d0100912c340eeef054
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-22 22:28:09 +0100

    12

--- vanilla/12-functions.js
+++ vanilla/12-functions.js
@@ -1,5 +1,22 @@
-function hello(name, age) {
-    console.log(`Cześć mam na imię ${x}, i mam ${y} lat`)
+// function hello(name, age) {
+//     console.log(`Cześć mam na imię ${x}, i mam ${y} lat`)
+// }
+
+// hello("Patrycja", 24);
+
+let score 
+
+function add(x, y) {
+    score = x + y;
+    score % 2 === 0 ? opt1 : opt2
+    }
+
+function opt1() {
+    console.log(`Liczba ${score} jest parzysta`)
 }
 
-hello("Patrycja", 24);
+function opt2() {
+    console.log(`Liczba ${score} jest nieparzysta`)
+}
+
+add(2, 4);
