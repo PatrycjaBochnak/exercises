c43eff3c7de32a8e504a09f85c20eee1e420b920
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-23 15:07:16 +0100

    13

---
+++ vanilla/13-dom.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector('h1');
+
+const p = document.querySelectorAll('p')
+p.push;
+
+const test1 = document.querySelector('.test')
+
+const pInsideDiv = document.querySelector('#test')
