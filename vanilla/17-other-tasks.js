154f05f180d8c9321c110d0f74dc8a16476c9cf5
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-31 19:12:29 +0200

    17

---
+++ vanilla/17-other-tasks.js
@@ -0,0 +1,7 @@
+const img = document.querySelector('img');
+
+img.setAttribute('src', 'https://unsplash.it/600/400')
+img.setAttribute('alt', 'losowy obrazek')
+const test = () => console.log('lol')
+
+setTimeout(test, 2000);
