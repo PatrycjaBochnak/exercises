e01ed4297ed75349a3a9d52051b68c7d7cdb6da1
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-03-31 19:22:36 +0200

    17

--- vanilla/17-other-tasks.js
+++ vanilla/17-other-tasks.js
@@ -5,3 +5,22 @@ img.setAttribute('alt', 'losowy obrazek')
 const test = () => console.log('lol')
 
 setTimeout(test, 2000);
+
+const allLiItems = document.querySelectorAll('li');
+let number = 1 
+
+// for(let allLiItems of liItems) {
+//    allLiItems.textContent = number
+//    liItem.dataset.id = number
+//    number++ 
+// }
+
+for (let i = 0; i <= liItems.length; i++) {
+    liItems[i].textContent = number
+    liItems[i].dataset.id = number
+    number++
+}
+
+const thirdLi = document.querySelector('[data-id="3"]');
+console.log(thirdLi.closest('wrapper'));
+
